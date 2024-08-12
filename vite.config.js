decbd905ee12f2b23b94565a7470129ca9a930e8
stephy-wen <nalsonlionmedia@gmail.com>
2024-08-12 17:58:36 +0800

    vite配置圖片預設路徑

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // 確保 @ 指向 src 資料夾
+    },
+  },
   server: {
     proxy: {
       '/api': {
