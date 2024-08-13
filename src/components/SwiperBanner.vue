754079862eaaf51ee70a1354b16592357c7165ee
stephy-wen <nalsonlionmedia@gmail.com>
2024-08-13 16:55:27 +0800

    首頁輪播引用 + 建構輪播組件

---
+++ src/components/SwiperBanner.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="swiper-banner justify-content-center mx-auto mt-0 mt-lg-5">
+    <Swiper
+      class="mySwiper"
+      :autoplay="{ delay: autoplayDelay }"
+      loop
+      :pagination="{ clickable: true }"
+      :scrollbar="{ draggable: true }"
+      :modules="[Autoplay, Pagination, Navigation]"
+    >
+      <SwiperSlide
+        v-for="(banner, index) in banners"
+        :key="index"
+        class="index-banner"
+      >
+        <a :href="banner.link">
+          <img class="img-fluid" :src="banner.image" :alt="banner.alt" />
+        </a>
+      </SwiperSlide>
+    </Swiper>
+  </div>
+</template>
+
+<script setup>
+import { Swiper, SwiperSlide } from "swiper/vue"; // Import Swiper Vue.js components
+import "swiper/css"; // Import Swiper styles
+import "swiper/css/pagination"; // Import Swiper pagination styles
+import "swiper/css/scrollbar"; // Import Swiper scrollbar styles
+import { Autoplay, Navigation, Pagination, Scrollbar } from "swiper/modules";
+// loop: 啟用幻燈片循環播放。
+// :pagination="{ clickable: true }": 顯示可點擊的分頁點。
+// :scrollbar="{ draggable: true }": 顯示可拖動的滾動條。
+// :modules="[Autoplay, Pagination, Navigation]": 指定 Swiper 使用的功能模組
+
+// Props definition
+const props = defineProps({
+  banners: {
+    type: Array,
+    required: true,
+  },
+  autoplayDelay: {
+    type: Number,
+    default: 3000,
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="swiper-banner justify-content-center mx-auto mt-0 mt-lg-5">
+      <SwiperBanner :banners="bannerData" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SwiperBanner from "@/components/SwiperBanner.vue"; // 引入輪播組件
+import banner01 from "@/assets/images/common/banner01.png"; // 引入圖片
+import banner02 from "@/assets/images/common/banner02.png"; // 引入圖片
+
+// 輪播圖片數據
+const bannerData = [
+  { image: banner01, link: "#", alt: "Banner 1" },
+  { image: banner02, link: "#", alt: "Banner 2" },
+];
+</script>
+
+<style scoped></style>
