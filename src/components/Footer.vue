47431a0a88177e55bbb2c560a96edc6803162b51
stephy-wen <nalsonlionmedia@gmail.com>
2024-08-13 16:55:43 +0800

    製作footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="mt-5" style="background-color: #181a20">
+    <hr class="w-75 mx-auto" />
+    <p>lionbin© 2024</p>
+  </footer>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* footer */
+
+footer {
+  background-color: #343a40;
+  color: #fff;
+  text-align: center;
+  padding: 20px 0;
+  margin-top: 40px;
+}
+</style>
