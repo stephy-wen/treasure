cd518ec1996a9e2dbcc42de8ad90adc99a407ef6
stephy-wen <nalsonlionmedia@gmail.com>
2024-08-20 17:51:59 +0800

    修復

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+
 import About from "../views/About.vue";
 import Index from "../views/index/index.vue";
 
