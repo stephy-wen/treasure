d6e70c8bab1a44817db3c52e3711efc0a6664f02
stephy-wen <nalsonlionmedia@gmail.com>
2024-08-21 17:25:04 +0800

    加入註解

--- src/router/index.js
+++ src/router/index.js
@@ -19,18 +19,18 @@ const routes = [
   {
     path: "/game/game-list",
     name: "Game",
-    component: () => import("../views/game/index.vue"), // 动态导入
+    component: () => import("../views/game/index.vue"), // 動態導入
   },
   {
-    path: "/game/:gameId", // 动态路由，使用游戏的 ID 作为参数
+    path: "/game/:gameId", // 動態路由，使用遊戲的 ID 作為參數
     name: "GameDetail",
-    component: () => import("../views/gameDetail/GameDetailPage.vue"), // 动态导入
-    props: true, // 传递路由参数作为组件的 props
+    component: () => import("../views/gameDetail/GameDetailPage.vue"), // 動態導入
+    props: true, // 傳遞路由參數作為組件的 props
   },
   {
     path: "/leaderboard",
     name: "Leaderboard",
-    component: () => import("../views/Leaderboard/index.vue"), // 动态导入
+    component: () => import("../views/Leaderboard/index.vue"), // 動態導入
   },
 ];
 
