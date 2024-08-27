c7c8de535f6968c3a17538f45818fc31b915f53b
stephy-wen <nalsonlionmedia@gmail.com>
2024-08-27 15:25:49 +0800

    縮排調整

--- src/views/gameDetail/components/JoinGameModal.vue
+++ src/views/gameDetail/components/JoinGameModal.vue
@@ -5,16 +5,18 @@
     class="modal fade show"
     id="joinModal"
     tabindex="-1"
-    aria-labelledby="joinModalLabel" 
+    aria-labelledby="joinModalLabel"
     aria-hidden="true"
     role="dialog"
     style="display: block"
   >
-    <div class="modal-dialog mx-auto modal-dialog-centered modal-fullscreen-sm-down">
-      <div 
+    <div
+      class="modal-dialog mx-auto modal-dialog-centered modal-fullscreen-sm-down"
+    >
+      <div
         class="modal-content modal-background"
         :style="{ backgroundImage: `url(${gameInfo.backgroundImage})` }"
-        > 
+      >
         <div class="modal-header pb-0 d-flex justify-content-between">
           <h5 class="modal-title" id="joinModalLabel"></h5>
           <button
@@ -22,13 +24,13 @@
             class="btn winnie-btn-close"
             @click="closeModal"
             aria-label="Close"
-            >
-            <font-awesome-icon
-            icon="fa-solid fa-xmark"
-            />
+          >
+            <font-awesome-icon icon="fa-solid fa-xmark" />
           </button>
         </div>
-        <div class="modal-body text-center d-flex flex-column justify-content-center">
+        <div
+          class="modal-body text-center d-flex flex-column justify-content-center"
+        >
           <div class="game-round-name">
             <p>Round {{ gameInfo.round }}</p>
             <p class="fs-2 fw-bold">{{ gameInfo.title }}</p>
@@ -36,22 +38,35 @@
           <div class="attend-fee mt-5">
             <p>Total Amount Deducted</p>
             <p class="fs-3 fw-bold">
-              <img class="me-2" style="width: 50px;" src="@/assets/images/icon/balance-icon.png" alt="">{{ gameInfo.vote }}
+              <img
+                class="me-2"
+                style="width: 50px"
+                src="@/assets/images/icon/balance-icon.png"
+                alt=""
+              />{{ gameInfo.vote }}
             </p>
           </div>
           <div class="attend-times-btn d-flex justify-content-center mt-2">
-          <button>1</button>
-          <button class="mx-3 mx-md-4">5</button>
-          <button>10</button>
+            <button>1</button>
+            <button class="mx-3 mx-md-4">5</button>
+            <button>10</button>
           </div>
           <div class="attend-times-input mt-5">
-          <span>x</span>
-          <input class="fs-6" value="1" type="text">
-          <!-- <span>Times</span> -->
+            <span>x</span>
+            <input class="fs-6" value="1" type="text" />
+            <!-- <span>Times</span> -->
           </div>
         </div>
         <div class="modal-footer mx-auto">
-        <button type="button" class="btn attend-confirm-btn mb-5" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#winnerModal">Confirm</button>
+          <button
+            type="button"
+            class="btn attend-confirm-btn mb-5"
+            data-bs-dismiss="modal"
+            data-bs-toggle="modal"
+            data-bs-target="#winnerModal"
+          >
+            Confirm
+          </button>
         </div>
       </div>
     </div>
@@ -86,86 +101,84 @@ const closeModal = () => {
 
 <style scoped>
 .winnie-btn-close {
-    background-color: #1E2329;
-    color: #F8F8F8;
-    border-radius: 50px;
-    border: none;
+  background-color: #1e2329;
+  color: #f8f8f8;
+  border-radius: 50px;
+  border: none;
 }
-
 
 .winnie-btn-close:hover {
-    background-color: #414D5A;
-    color: #F8F8F8;
+  background-color: #414d5a;
+  color: #f8f8f8;
 }
 
-
 .game-attend .modal-dialog {
-    width: 100%;
+  width: 100%;
 }
 
 @media (min-width: 575.98px) {
-    .game-attend .modal-dialog {
-        max-width: 400px;
-    }
+  .game-attend .modal-dialog {
+    max-width: 400px;
+  }
 }
 
 #joinModal .modal-background {
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 
 #joinModal .modal-content {
-    color: #F8F8F8;
-    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border: none;
+  color: #f8f8f8;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: none;
 }
 
 #joinModal .attend-times-btn button {
-    background-color: transparent;
-    border: 1px solid #F8F8F8;
-    padding: 8px 30px;
-    color: #F8F8F8;
-    border-radius: 10px;
+  background-color: transparent;
+  border: 1px solid #f8f8f8;
+  padding: 8px 30px;
+  color: #f8f8f8;
+  border-radius: 10px;
 }
 
 #joinModal .attend-times-btn button:hover {
-    background-color: rgba(0, 0, 0, 0.25);
+  background-color: rgba(0, 0, 0, 0.25);
 }
 
 #joinModal .modal-header {
-    border-bottom: none;
+  border-bottom: none;
 }
 
 #joinModal .modal-footer {
-    border-top: none;
+  border-top: none;
 }
 
 #joinModal .modal-content {
-    background-color: transparent;
+  background-color: transparent;
 }
 
 #joinModal .modal-body input {
-    background-color: transparent;
-    border: none;
-    border-bottom: 1px solid #F8F8F8;
-    color: #F8F8F8;
-    text-align: center;
-    border-radius: 0px;
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid #f8f8f8;
+  color: #f8f8f8;
+  text-align: center;
+  border-radius: 0px;
 }
 
 .game-attend #joinModal .modal-body input:focus {
-    outline: none;
+  outline: none;
 }
 
 .game-attend #joinModal .modal-footer button.attend-confirm-btn {
-    background-color: #FCD535;
-    border: none;
-    color: #181A20;
-    padding: 8px 90px;
+  background-color: #fcd535;
+  border: none;
+  color: #181a20;
+  padding: 8px 90px;
 }
 
 .game-attend #joinModal .modal-footer button.attend-confirm-btn:hover {
-    background-color: #E5BF30;
+  background-color: #e5bf30;
 }
 </style>
