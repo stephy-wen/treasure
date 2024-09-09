bcd912a22be4223372f4ba142bad0f0c2f4fa73f
stephy-wen <nalsonlionmedia@gmail.com>
2024-09-09 15:23:53 +0800

    整理遊戲頁面

--- src/views/gameDetail/GameDetailPage.vue
+++ src/views/gameDetail/GameDetailPage.vue
@@ -53,16 +53,23 @@ const {
   game: { getGameRoom, getGameWinnerHistory },
 } = modules;
 
-// 監聽螢幕變化
 const isMobile = ref(window.innerWidth < 575.98);
-const handleResize = () => {
-  isMobile.value = window.innerWidth < 575.98;
-};
-
 const gameDetails = ref({}); // 初始化遊戲資料
 const votes = ref(null); // 使用 ref 來讓 votes 成為響應式變量
 const maxVotes = ref(null);
 const route = useRoute(); // 獲取路由對象
+
+// 項目總數和每頁項目數
+const gid = ref(null); // 用來保存遊戲資料中的 gid
+const currentItems = ref([]); // 當前頁面的數據
+const totalItems = ref(0); // 總項目數
+const itemsPerPage = ref(5); // 每頁顯示 5 筆
+const pageCache = ref({}); // 緩存每一頁的資料
+
+// 處理視窗大小變化
+const handleResize = () => {
+  isMobile.value = window.innerWidth < 575.98;
+};
 
 // 定義重新獲取遊戲資料的函數
 const loadGameDetails = async () => {
@@ -75,7 +82,6 @@ const loadGameDetails = async () => {
       maxVotes.value = response.data.data.maxQuantity;
       gid.value = response.data.data.gId;
       gameDetails.value = response.data.data; // 更新遊戲資料
-      console.log("loadGameDetails 中的 gid:", gid.value); // 確認 gid 已正確賦值
     } else {
       console.error("未獲取到有效的遊戲資料");
     }
@@ -83,6 +89,75 @@ const loadGameDetails = async () => {
     console.error("獲取遊戲資料時發生錯誤：", error);
   }
 };
+
+// 歷史資料
+const fetchPageData = async (pageIndex) => {
+  try {
+    if (!gid.value) {
+      console.error("gid 尚未獲取到，無法加載歷史資料");
+      return;
+    }
+    // 檢查是否有緩存
+    if (pageCache.value[pageIndex]) {
+      currentItems.value = pageCache.value[pageIndex];
+      return;
+    }
+    // 調用 API 獲取歷史資料
+    const response = await getGameWinnerHistory(
+      gid.value,
+      itemsPerPage.value,
+      pageIndex
+    );
+
+    if (response && response.data.data) {
+      currentItems.value = formatHistoryData(response.data.data.items); // 該頁資料的整理
+      totalItems.value = response.data.data.total; // 總筆數
+    } else {
+      console.error("未獲取到有效的歷史資料");
+    }
+  } catch (error) {
+    console.error("獲取歷史時發生錯誤：", error);
+  }
+};
+
+const formatHistoryData = (data) => {
+  return data.map((winner) => [
+    {
+      text: winner.round,
+      class: "text-center",
+      image: null,
+    },
+    {
+      text: winner.winner,
+      class: "leaderboard-name",
+      image: winner.winnerIconUrl,
+    },
+    {
+      text: formatDate(winner.transactionDateTime),
+      class: "d-none d-sm-table-cell text-end pe-3 pe-sm-0",
+      image: null,
+    },
+    {
+      text: null,
+      class: "text-end pe-3 pe-sm-5",
+      image: verify,
+      link: winner.transactionUrl,
+      target: "_blank",
+    },
+  ]);
+};
+
+// ------------ 歷史資料顯示用的header配置 ------------
+const headers = [
+  { text: "Round", class: "text-center" },
+  { text: "Winner" },
+  { text: "Time", class: "d-none d-sm-table-cell text-end pe-3 pe-sm-0" },
+  { text: "Verify", class: "text-end pe-3 pe-sm-5" },
+];
+
+// 日期格式化函數
+const formatDate = (isoDateString) =>
+  dayjs(isoDateString).format("YYYY/MM/DD HH:mm:ss");
 
 // 初始化時調用一次，獲取遊戲資料
 onMounted(async () => {
@@ -110,88 +185,6 @@ onBeforeUnmount(() => {
 provide("gameDetails", gameDetails); // 傳遞 ref 對象，而不是 .value
 provide("votes", votes); // 傳遞 ref 對象
 provide("maxVotes", maxVotes); // 傳遞 ref 對象
-
-// ------------ 分頁與歷史數據處理 ------------
-
-// 項目總數和每頁項目數
-const currentItems = ref([]); // 當前頁面的數據
-const totalItems = ref(0); // 總項目數
-const itemsPerPage = ref(5); // 每頁顯示 5 筆
-const gid = ref(null); // 用來保存遊戲資料中的 gid
-const pageCache = ref({}); // 緩存每一頁的資料
-
-// 模擬從後端獲取數據的函數
-const fetchPageData = async (pageIndex) => {
-  try {
-    if (!gid.value) {
-      console.error("gid 尚未獲取到，無法加載贏家歷史");
-      return;
-    }
-    // 檢查是否有緩存
-    if (pageCache.value[pageIndex]) {
-      currentItems.value = pageCache.value[pageIndex];
-      return;
-    }
-    // 調用 API 獲取贏家歷史資料
-    const response = await getGameWinnerHistory(
-      gid.value,
-      itemsPerPage.value,
-      pageIndex
-    );
-
-    if (response && response.data.data) {
-      currentItems.value = formatHistoryData(response.data.data.items); // 更新當前頁的贏家歷史數據
-      totalItems.value = response.data.data.total; // 更新總贏家數，用來計算總頁數
-      console.log("response.data", response.data);
-      console.log("總共幾筆", totalItems.value);
-      console.log(response.data.data, "response.data.data");
-    } else {
-      console.error("未獲取到有效的贏家歷史資料");
-    }
-  } catch (error) {
-    console.error("獲取贏家歷史時發生錯誤：", error);
-  }
-};
-
-// 定義資料格式化函數
-const formatHistoryData = (data) => {
-  return data.map((winner) => [
-    {
-      text: winner.round, // 回合
-      class: "text-center",
-      image: null,
-    },
-    {
-      text: winner.winner, // 贏家名字
-      class: "leaderboard-name",
-      image: winner.winnerIconUrl, // 贏家頭像 URL
-    },
-    {
-      text: formatDate(winner.transactionDateTime), // 比賽時間
-      class: "d-none d-sm-table-cell text-end pe-3 pe-sm-0",
-      image: null,
-    },
-    {
-      text: null, // 驗證圖片
-      class: "text-end pe-3 pe-sm-5",
-      image: verify, // 使用 API 返回的驗證圖片 URL 或默認圖片
-      link: winner.transactionUrl, // API 返回的交易鏈接
-      target: "_blank", // 新窗口打開
-    },
-  ]);
-};
-
-// ------------ 歷史資料顯示用的header配置 ------------
-const headers = [
-  { text: "Round", class: "text-center" },
-  { text: "Winner" },
-  { text: "Time", class: "d-none d-sm-table-cell text-end pe-3 pe-sm-0" },
-  { text: "Verify", class: "text-end pe-3 pe-sm-5" },
-];
-
-// 日期格式化函數
-const formatDate = (isoDateString) =>
-  dayjs(isoDateString).format("YYYY/MM/DD HH:mm:ss");
 </script>
 
 <style scoped>
