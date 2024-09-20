7d73658c38f81d87798e13cd027684368424d65c
stephy-wen <nalsonlionmedia@gmail.com>
2024-09-20 10:47:37 +0800

    terms

--- src/views/terms/index.vue
+++ src/views/terms/index.vue
@@ -1,7 +1,316 @@
 <template>
-  <div>terms-of-service</div>
+  <div class="container winnie-terms mt-5 px-3 px-lg-0">
+    <!-- Side Navigation for Desktop -->
+    <nav class="side-nav fs-6 fw-bold">
+      <ul>
+        <li><a href="#introduction">1. Introduction</a></li>
+        <li>
+          <a href="#modificationAgreement">2. Modification of this Agreement</a>
+        </li>
+        <li><a href="#conditionsOfUse">3. Conditions of use</a></li>
+        <li><a href="#disclaimers">4. Disclaimers</a></li>
+        <li><a href="#proprietaryRights">5. Proprietary Rights</a></li>
+        <li><a href="#eligibility">6. Eligibility</a></li>
+        <li><a href="#privacy">7. Privacy</a></li>
+        <li><a href="#prohibitedActivity">8. Prohibited Activity</a></li>
+        <li><a href="#limitationOfLiability">9. Limitation of Liability</a></li>
+      </ul>
+    </nav>
+
+    <!-- Main Content -->
+    <div class="container content">
+      <h1 class="mb-3 fw-bold">Terms of Service</h1>
+      <p>Updated December 05, 2023</p>
+      <div
+        class="accordion accordion-flush d-md-none"
+        id="accordionFlushExample"
+      >
+        <div class="accordion-item">
+          <h2 class="accordion-header fw-bold" id="flush-headingOne">
+            <button
+              class="accordion-button collapsed pb-0 px-0 fs-5 fw-bold"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
+            >
+              <i class="fa-solid fa-list pe-2"></i>On this page
+            </button>
+          </h2>
+          <div
+            id="flush-collapseOne"
+            class="accordion-collapse collapse"
+            aria-labelledby="flush-headingOne"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body fs-6 fw-bold">
+              <ul>
+                <li class="my-2">
+                  <a href="#introduction">1. Introduction</a>
+                </li>
+                <li class="my-2">
+                  <a href="#modificationAgreement"
+                    >2. Modification of this Agreement</a
+                  >
+                </li>
+                <li class="my-2">
+                  <a href="#conditionsOfUse">3. Conditions of use</a>
+                </li>
+                <li class="my-2"><a href="#disclaimers">4. Disclaimers</a></li>
+                <li class="my-2">
+                  <a href="#proprietaryRights">5. Proprietary Rights</a>
+                </li>
+                <li class="my-2"><a href="#eligibility">6. Eligibility</a></li>
+                <li class="my-2"><a href="#privacy">7. Privacy</a></li>
+                <li class="my-2">
+                  <a href="#prohibitedActivity">8. Prohibited Activity</a>
+                </li>
+                <li class="my-2">
+                  <a href="#limitationOfLiability"
+                    >9. Limitation of Liability</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="introduction" class="my-5">
+        <h2 class="mb-3 fw-bold">1. Introduction</h2>
+        <p>
+          HUNTER Games is an onchain gaming interface maintained by a
+          decentralized team of developers. It facilitates interaction with the
+          HUNTER Games smart contracts deployed on the Blast blockchain.<br /><br />These
+          Terms of Service govern your use of the HUNTER Games website (the
+          “Website”) and any associated services (the “Services”).<br /><br />Win
+          or lose in our suite of crypto games, you’re getting rewarded. With
+          every move you make, your pile of Points only grows — and your
+          leaderboard ranking increases. Complete daily tasks and hit new
+          milestones to progress, then take home your cut of a huge reward at
+          the end of every season.
+        </p>
+      </div>
+      <div id="modificationAgreement" class="my-5">
+        <h2 class="mb-3 fw-bold">2. Modification of this Agreement</h2>
+        <p>
+          We reserve the right, in our sole discretion, to modify this
+          Agreement. All modifications become effective when they are posted,
+          and we will notify you by updating the date at the top of the
+          Agreement.<br /><br />You can find the latest version in effect at:
+        </p>
+      </div>
+      <div id="conditionsOfUse" class="my-5">
+        <h2 class="mb-3 fw-bold">3. Conditions of use</h2>
+        <p>
+          3.1. As a condition of your use of the Services, you represent,
+          warrant, covenant and agree that:<br /><br />
+          1. Your use of the Services is at your sole option, discretion and
+          risk;<br /><br />2. You acknowledge that HUNTER Games does not at any
+          time hold any user funds and that all interactions are directly from
+          your digital wallet;<br /><br />3. You acknowledge that all game
+          results and payouts are computed directly via smart contracts deployed
+          on the Blast blockchain;<br /><br />4. You accept and acknowledge that
+          the prevailing market value of cryptocurrencies can change
+          dramatically;<br /><br />5. You have not been diagnosed or classified
+          as a compulsive or problem gambler;<br /><br />6. You are not
+          currently self-excluded from any gambling sites or gambling
+          premises,<br /><br />7. You participate in games provided by HUNTER
+          Games on your own behalf and not on behalf of any other person;<br /><br />8.
+          You participate in games provided by HUNTER Games strictly in your
+          personal and non-professional capacity and for recreational and
+          entertainment purposes only;<br /><br />9. You are accessing the
+          Website from a jurisdiction in which it is legal to do so, and will
+          not use our Services while located in any jurisdiction that prohibits
+          the placing and/or accepting of bets online and/or playing casino
+          and/or live games or which otherwise prohibits access to or use of the
+          Services;<br /><br />10. You acknowledge that we are not liable for
+          any outages, slowness, capacity constraints or other deficiencies
+          affecting the telecommunications networks or internet services
+          required for you to access and use the Services;
+        </p>
+      </div>
+      <div id="disclaimers" class="my-5">
+        <h2 class="mb-3 fw-bold">4. Disclaimers</h2>
+        <p>
+          We do not represent or warrant that access to the front-end interface
+          will be continuous, uninterrupted, timely, or secure; that the
+          information contained in the interface will be accurate, reliable,
+          complete, or current; or that the Interface will be free from errors,
+          defects, viruses, or other harmful elements.
+        </p>
+      </div>
+      <div id="proprietaryRights" class="my-5">
+        <h2 class="mb-3 fw-bold">5. Proprietary Rights</h2>
+        <p>
+          We own the intellectual property generated by core contributors to
+          HUNTER Games for the use of HUNTER Games, including (but not limited
+          to) software, text, designs, images, and copyrights. Unless otherwise
+          stated, HUNTER Games reserves exclusive rights to its intellectual
+          property.
+        </p>
+      </div>
+      <div id="eligibility" class="my-5">
+        <h2 class="mb-3 fw-bold">6. Eligibility</h2>
+        <p>
+          To access or use the front-end interface, you represent that you are
+          at least the age of majority in your jurisdiction. You further
+          represent that your access and use of the front-end interface will
+          fully comply with all applicable laws and regulations and that you
+          will not access or use the front-end interface to conduct, promote, or
+          otherwise facilitate any illegal activity.<br /><br />
+          Furthermore, you represent that neither you nor any entity you
+          represent are included in any trade embargoes or sanctions list
+          (“Subject to Restrictions”), nor resident, citizen, national or agent
+          of, or an entity organized, incorporated or doing business in such
+          territories (“Restricted Territories”).
+        </p>
+      </div>
+      <div id="privacy" class="my-5">
+        <h2 class="mb-3 fw-bold">7. Privacy</h2>
+        <p>
+          When you use the front-end interface, the only information we collect
+          from you is your blockchain wallet address, completed transaction
+          hashes, and token identifiers. We do not collect any personal
+          information from you. We do, however, use third-party services like
+          Google Analytics, which may receive your publicly available personal
+          information. We do not take responsibility for any information you
+          make public on the Blast blockchain by taking actions through the
+          front-end interface.
+        </p>
+      </div>
+      <div id="privacy" class="my-5">
+        <h2 class="mb-3 fw-bold">8. Prohibited Activity</h2>
+        <p>
+          You agree not to engage in any of the following categories of
+          prohibited activity in relation to your access and use of the
+          front-end interface: Transacting in any Restricted Territory or
+          interacting with any blockchain addresses controlled indirectly or
+          directly by persons or entities Subject to Restrictions, that is,
+          included in any trade embargoes or sanctions list.
+        </p>
+      </div>
+      <div id="privacy" class="my-5">
+        <h2 class="mb-3 fw-bold">9. Limitation of Liability</h2>
+        <p>
+          HUNTER Games is in no way liable for any damages of any form resulting
+          from your access or use of HUNTER Games software, including (but not
+          limited to) any loss of profit, digital assets, or intangible
+          property, and assumes no liability or responsibility for any errors,
+          omissions, mistakes, or inaccuracies in the content provided on HUNTER
+          Games-controlled software or media;<br /><br />
+          unauthorized access or use of any server or database controlled by
+          HUNTER Games; bugs, viruses etc. in the software; suspension of
+          service; or any conduct of any third party whatsoever. Furthermore,
+          any hyperlink or reference to a third party website, product, or
+          person that is shared or published in any software or other channel by
+          HUNTER Games is for your convenience only, and does not constitute an
+          endorsement. We accept no legal responsibility for content or
+          information of such third party sites.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+.winnie-terms .side-nav {
+  position: fixed;
+  top: 200px;
+  left: 200px;
+  height: 100%;
+  width: 250px;
+  padding-top: 20px;
+  background-color: transparent;
+  display: none;
+}
+.winnie-terms .side-nav a {
+  display: block;
+  padding: 10px 15px;
+  color: #f8f8f8;
+  text-decoration: none;
+}
+
+.winnie-terms .side-nav a:hover {
+  color: #fcd535;
+}
+.winnie-terms .content {
+  margin-left: 0;
+}
+@media (min-width: 768px) {
+  .winnie-terms .side-nav {
+    display: block;
+  }
+  .winnie-terms .content {
+    margin-left: 250px;
+  }
+}
+
+@media (min-width: 768px) and (max-width: 991.98px) {
+  .winnie-terms.container {
+    width: 80%;
+  }
+}
+@media (min-width: 991.98px) and (max-width: 1199.98px) {
+  .winnie-terms.container {
+    width: 70%;
+  }
+}
+@media (min-width: 768px) and (max-width: 1699.98px) {
+  .winnie-terms .side-nav {
+    left: 80px;
+  }
+}
+
+.winnie-terms .accordion {
+  --bs-accordion-btn-bg: transparent;
+  --bs-accordion-bg: transparent;
+  --bs-accordion-color: #f8f8f8;
+  --bs-accordion-btn-color: #f8f8f8;
+  --bs-accordion-active-bg: transparent;
+  --bs-accordion-btn-focus-box-shadow: none;
+  --bs-accordion-border-color: transparent;
+}
+
+.winnie-terms .accordion a {
+  color: #f8f8f8;
+}
+
+.winnie-terms .accordion-button:not(.collapsed) {
+  color: #f8f8f8;
+}
+
+.winnie-terms .fa-list {
+  color: #f8f8f8;
+}
+
+.winnie-terms .side-nav a {
+  border-right: 1px solid #f8f8f8;
+}
+
+.winnie-terms .side-nav a:hover,
+.winnie-terms .side-nav a:focus {
+  border-right: 3px solid #fcd535;
+}
+
+.winnie-terms .accordion-body ul {
+  border-right: 1px solid #f8f8f8;
+}
+
+.winnie-terms .accordion-body li:hover,
+.winnie-terms .accordion-body li:focus {
+  border-right: 3px solid #fcd535;
+}
+
+.winnie-terms .accordion-body li:hover a,
+.winnie-terms .accordion-body li:focus a {
+  color: #fcd535;
+}
+
+.winnie-terms p {
+  color: #bbb;
+}
+</style>
