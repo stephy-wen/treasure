863537930bf10efb0b19abd84620ea3379c2413c
stephy-wen <nalsonlionmedia@gmail.com>
2024-09-23 11:30:36 +0800

    定義共用api error 寫到一半(之後再改)

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,5 +1,27 @@
 import { ElMessage } from "element-plus";
 
+export const handleError = (error) => {
+  let message = "失敗，請稍後再試。"; // 預設錯誤訊息
+
+  const errorMessages = {
+    2005: "驗證碼不正確",
+    2006: "驗證碼已過期",
+    2021: "推薦碼錯誤",
+    4001: "請求無效，請檢查輸入資訊",
+    5000: "伺服器內部錯誤，請稍後再試",
+  };
+
+  if (error.response && error.response.data.message) {
+    const systemCode = error.response.data.systemCode;
+
+    message =
+      errorMessages[systemCode] || error.response.data.message || message;
+    return message;
+  } else {
+    return "網絡錯誤，請稍後再試。";
+  }
+};
+
 export const handleApiError = (error) => {
   if (error.response && error.response.data.message) {
     return error.response.data.message;
